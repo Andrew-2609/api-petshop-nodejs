86e394e3597ad9e5ce836cb137f40289af661151
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 23:21:45 -0300

    Feat: add code to create table 'fornecedores' in criarTabelas.js

---
+++ api/banco-de-dados/criarTabelas.js
@@ -0,0 +1,8 @@
+const ModeloTabela = require('../rotas/fornecedores/ModeloTabelaFornecedor');
+
+ModeloTabela.sync()
+    .then(() => {
+        console.log('Tabela "fornecedores" criada com sucesso!')
+    })
+    .catch(console.log)
+;
