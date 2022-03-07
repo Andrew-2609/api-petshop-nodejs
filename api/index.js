d6933390a7dcc3d23c863f1979afd63801f95cf4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-07 09:25:18 -0300

    Feat: replace bodyPaser.json() with express.json()

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const config = require('config');
 const roteador = require('./rotas/fornecedores');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api/fornecedores', roteador);
 
