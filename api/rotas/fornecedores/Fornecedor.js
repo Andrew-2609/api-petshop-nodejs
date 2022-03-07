1557fa1fd89f6937bf129efb52c31fee3b3fc177
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 23:51:48 -0300

    Feat: implement method criar() in Fornecedor.js

--- api/rotas/fornecedores/Fornecedor.js
+++ api/rotas/fornecedores/Fornecedor.js
@@ -1,12 +1,27 @@
+const TabelaFornecedor = require('./TabelaFornecedor');
+
 class Fornecedor {
     constructor({id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao}) {
-        this._id = id;
-        this._empresa = empresa;
-        this._email = email;
-        this._categoria = categoria;
-        this._dataCriacao = dataCriacao;
-        this._dataAtualizacao = dataAtualizacao;
-        this._versao = versao;
+        this.id = id;
+        this.empresa = empresa;
+        this.email = email;
+        this.categoria = categoria;
+        this.dataCriacao = dataCriacao;
+        this.dataAtualizacao = dataAtualizacao;
+        this.versao = versao;
+    }
+
+    async criar() {
+        const resultado = await TabelaFornecedor.inserir({
+            empresa: this.empresa,
+            email: this.email,
+            categoria: this.categoria
+        });
+
+        this.id = resultado.id;
+        this.dataCriacao = resultado.dataCriacao;
+        this.dataAtualizacao = resultado.dataAtualizacao;
+        this.versao = resultado.versao;
     }
 }
 
