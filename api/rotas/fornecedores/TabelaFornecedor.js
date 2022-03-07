f57c7e901a90f2176abc3eab13bb8ff6ade3a16a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-07 16:08:36 -0300

    Feat: implement async function pegarPorId() in TabelaFornecedor

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -4,6 +4,17 @@ module.exports = {
     listar() {
         return Modelo.findAll();
     },
+    async pegarPorId(id) {
+        const fornecedorEncontrado = await Modelo.findOne({
+            where: {id: id}
+        });
+
+        if (!fornecedorEncontrado) {
+            throw new Error('Fornecedor não encontrado.');
+        }
+
+        return fornecedorEncontrado;
+    },
     inserir(fornecedor) {
         return Modelo.create(fornecedor);
     }
