7c55b85393cd1b9e7a41938463d192fe12805f18
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-08 21:54:34 -0300

    Chore: implement basic structure of CampoInvalidoError.js

---
+++ api/erros/CampoInvalidoError.js
@@ -0,0 +1,10 @@
+class CampoInvalidoError extends Error {
+    constructor(campo) {
+        const mensagem = `O campo '${campo}' passado é inválido`;
+        super(mensagem);
+        this.name = 'CampoInvalidoError';
+        this.idErro = 1;
+    }
+}
+
+module.exports = CampoInvalidoError;
