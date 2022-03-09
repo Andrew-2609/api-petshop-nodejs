09a36240b49da92bebc25f9e11cf525b3f79acb1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-08 22:11:33 -0300

    Chore: change the idErro value of DadosNaoFornecidosError

--- api/erros/DadosNaoFornecidosError.js
+++ api/erros/DadosNaoFornecidosError.js
@@ -2,7 +2,7 @@ class DadosNaoFornecidosError extends Error {
     constructor() {
         super('Não foram fornecidos dados para atualizar!');
         this.name = 'DadosNaoFornecidosError';
-        this.idErro = 3;
+        this.idErro = 2;
     }
 }
 
