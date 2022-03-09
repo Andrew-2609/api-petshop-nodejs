bbbb7ff131c24f94ea2d69331e003cc66b9ac7fc
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-09 00:35:31 -0300

    Feat: add method filtrarObjeto() to filter the response object

--- api/Serializador.js
+++ api/Serializador.js
@@ -2,10 +2,11 @@ const ValorNaoSuportadoError = require('./erros/ValorNaoSuportadoError');
 
 class Serializador {
     contentType;
+    camposPublicos;
 
     serializar(dados) {
         if (this.contentType === 'application/json') {
-            return this.json(dados);
+            return this.json(this.filtrarObjeto(dados));
         }
 
         throw new ValorNaoSuportadoError(this.contentType);
@@ -14,12 +15,25 @@ class Serializador {
     json(dados) {
         return JSON.stringify(dados);
     }
+
+    filtrarObjeto(dados) {
+        const objetoResposta = {};
+
+        this.camposPublicos.forEach(campo => {
+            if (dados.hasOwnProperty(campo)) {
+                objetoResposta[campo] = dados[campo];
+            }
+        });
+
+        return objetoResposta;
+    }
 }
 
 class SerializadorFornecedor extends Serializador {
     constructor(contentType) {
         super();
         this.contentType = contentType;
+        this.camposPublicos = ['id', 'empresa', 'categoria'];
     }
 }
 
