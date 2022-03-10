d5cea86c1a3dd923cca07d8f0bd33b0755dbc6fd
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-10 00:19:24 -0300

    Feat: implement method criar()

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -1,3 +1,5 @@
+const TabelaProduto = require('./TabelaProduto');
+
 class Produto {
     constructor({id, titulo, preco, estoque, idFornecedor, dataCriacao, dataAtualizacao, versao}) {
         this.id = id;
@@ -8,6 +10,20 @@ class Produto {
         this.dataCriacao = dataCriacao;
         this.dataAtualizacao = dataAtualizacao;
         this.versao = versao;
+    }
+
+    async criar() {
+        const resultado = await TabelaProduto.inserir({
+            titulo: this.titulo,
+            preco: this.preco,
+            estoque: this.estoque,
+            idFornecedor: this.idFornecedor
+        });
+
+        this.id = resultado.id;
+        this.dataCriacao = resultado.dataCriacao;
+        this.dataAtualizacao = resultado.dataAtualizacao;
+        this.versao = resultado.versao;
     }
 }
 
