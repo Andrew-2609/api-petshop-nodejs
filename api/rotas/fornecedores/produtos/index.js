bed8ff6a9ad26a92e4cb7e703b9b053724112eef
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-11 15:09:12 -0300

    Feat: implement POST endpoint to create new products

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,23 @@
 const roteador = require('express').Router({mergeParams: true});
 const TabelaProduto = require('./TabelaProduto');
+const Produto = require('./Produto');
 
 roteador.get('/', async (req, res) => {
-    const produtos = await TabelaProduto.listar(req.params.idFornecedor);
+    const produtos = await TabelaProduto.listar(req.params['idFornecedor']);
+    res.status(200);
     res.send(
         JSON.stringify(produtos)
     );
+});
+
+roteador.post('/', async (req, res) => {
+    const idFornecedor = req.params['idFornecedor'];
+    const corpo = req.body;
+    const dados = Object.assign({}, corpo, {idFornecedor: idFornecedor});
+    const produto = new Produto(dados);
+    await produto.criar();
+    res.status(201);
+    res.send(JSON.stringify(produto));
 });
 
 module.exports = roteador;
