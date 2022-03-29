5ffdfca5703efaf9dd1f9e61384a883ac621bcd1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-29 16:36:03 -0300

    Feat: replace req.params call with req.fornecedor based on new middleware implementation

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,9 +1,9 @@
-const roteador = require('express').Router({mergeParams: true});
+const roteador = require('express').Router({ mergeParams: true });
 const TabelaProduto = require('./TabelaProduto');
 const Produto = require('./Produto');
 
 roteador.get('/', async (req, res) => {
-    const produtos = await TabelaProduto.listar(req.params['idFornecedor']);
+    const produtos = await TabelaProduto.listar(req.fornecedor.id);
     res.status(200);
     res.send(
         JSON.stringify(produtos)
@@ -12,7 +12,7 @@ roteador.get('/', async (req, res) => {
 
 roteador.get('/:idProduto', async (req, res) => {
     const idProduto = req.params['idProduto'];
-    const produto = new Produto({id: idProduto});
+    const produto = new Produto({ id: idProduto });
     await produto.carregar();
     res.status(200);
     res.send(JSON.stringify(produto));
@@ -20,9 +20,9 @@ roteador.get('/:idProduto', async (req, res) => {
 
 roteador.post('/', async (req, res, next) => {
     try {
-        const idFornecedor = req.params['idFornecedor'];
+        const idFornecedor = req.fornecedor.id;
         const corpo = req.body;
-        const dados = Object.assign({}, corpo, {idFornecedor: idFornecedor});
+        const dados = Object.assign({}, corpo, { idFornecedor: idFornecedor });
         const produto = new Produto(dados);
         await produto.criar();
         res.status(201);
@@ -34,7 +34,7 @@ roteador.post('/', async (req, res, next) => {
 
 roteador.delete('/:idProduto', async (req, res) => {
     const idProduto = req.params['idProduto'];
-    const produto = new Produto({id: idProduto});
+    const produto = new Produto({ id: idProduto });
     await produto.deletar();
     res.status(204);
     res.end();
