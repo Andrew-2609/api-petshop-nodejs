88ccfb39a7a02dbc80000e2b99013da1473e2281
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-30 07:55:05 -0300

    Refactor: replace logic of Error handling in api/index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 const config = require('config');
 const roteador = require('./rotas/fornecedores');
-const FornecedorNaoEncontradoError = require('./erros/FornecedorNaoEncontradoError');
-const CampoInvalidoError = require('./erros/CampoInvalidoError');
-const DadosNaoFornecidosError = require('./erros/DadosNaoFornecidosError');
-const ValorNaoSuportadoError = require('./erros/ValorNaoSuportadoError');
-const {SerializadorErro, formatosAceitos} = require('./Serializador');
-const EstoqueAbaixoDeZeroError = require('./erros/EstoqueAbaixoDeZeroError.js');
+const { SerializadorErro, formatosAceitos } = require('./Serializador');
 
 app.use(express.json());
 
@@ -31,18 +26,9 @@ app.use((req, res, next) => {
 app.use('/api/fornecedores', roteador);
 
 app.use((erro, req, res, next) => {
-    let codigoStatus = 500;
-
-    codigoStatus = erro instanceof CampoInvalidoError || erro instanceof DadosNaoFornecidosError || erro instanceof EstoqueAbaixoDeZeroError
-        ? 400
-        : codigoStatus
-    ;
-    codigoStatus = erro instanceof FornecedorNaoEncontradoError ? 404 : codigoStatus;
-    codigoStatus = erro instanceof ValorNaoSuportadoError ? 406 : codigoStatus;
-
     const serializador = new SerializadorErro(res.getHeader('Content-Type'));
 
-    res.status(codigoStatus);
+    res.status(erro.status || 500);
     res.send(serializador.serializar({
         mensagem: erro.message,
         id: erro.idErro
